850d2e44c523c74f4e0ad04bc74d48f970d597b3
Viredon <eriksenly@icloud.com>
2025-03-17 18:13:59 +0100

    Added customer service and controller

---
+++ SeaEco.Services/CustomerServices/ICustomerService.cs
@@ -0,0 +1,10 @@
+﻿using SeaEco.Abstractions.Models.Customer;
+using SeaEco.EntityFramework.Entities;
+
+namespace SeaEco.Services.CustomerServices;
+
+public interface ICustomerService
+{
+    Task<List<CustomerNamesDto>> GetCustomerNames();
+    Task<Kunde?> GetCustomerById(int customerId);
+}
