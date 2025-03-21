6e37e53107c12592f11297077e671de8c5b6771f
shv006 <97917772+shv006@users.noreply.github.com>
2025-03-21 16:32:02 +0100

    added login functionality and environment variable handling
    # Conflicts:
    #	SeaEco.Server/Program.cs

--- SeaEco.Client/Program.cs
+++ SeaEco.Client/Program.cs
@@ -1,11 +1,19 @@
+using System.Net.Http.Json;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SeaEco.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var http = new HttpClient{BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)};
+builder.Services.AddScoped(sp => http);
+
+var config = await http.GetFromJsonAsync<Dictionary<string, string>>("appsettings.json");
+builder.Services.AddSingleton(config);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
 await builder.Build().RunAsync();
+
+
