411e140c78c1029819b8d0014ed912783cb935ab
Tetiana <tda072@uit.no>
2025-03-25 21:20:06 +0100

    feat(db): update BBilder table structure with necessary schema changes

---
+++ SeaEco.EntityFramework/Migrations/20250325192155_ChangedExtensionTypeInBBilder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeaEco.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangedExtensionTypeInBBilder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "extension",
+                table: "b_bilder",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldType: "uuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "extension",
+                table: "b_bilder",
+                type: "uuid",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
---
+++ SeaEco.EntityFramework/Migrations/20250325195906_AddedSiltToBBilder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeaEco.EntityFramework.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedSiltToBBilder : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "silt",
+                table: "b_bilder",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "silt",
+                table: "b_bilder");
+        }
+    }
+}
