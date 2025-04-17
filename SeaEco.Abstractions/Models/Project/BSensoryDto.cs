bcfd5b29918e1afd6221d3947bc117988754277f
Isak Eriksen <eriksenly@icloud.com>
2025-04-17 02:30:08 +0200

    Frontend "Seditmenttype" and buttons

--- SeaEco.Abstractions/Models/Project/BSensoryDto.cs
+++ SeaEco.Abstractions/Models/Project/BSensoryDto.cs
@@ -4,21 +4,21 @@ namespace SeaEco.Abstractions.Models.Project;
 
 public class BSensoryDto
 {
-    public int ProvePh { get; set; }
+    public double? ProvePh { get; set; }
 
-    public int ProveEh { get; set; }
+    public double? ProveEh { get; set; }
 
-    public int ProveTemp { get; set; }
+    public double? ProveTemp { get; set; }
     
-    public bool Farge { get; set; }
+    public int? Farge { get; set; }
 
-    public int Lukt { get; set; }
+    public int? Lukt { get; set; }
 
-    public int Konsistens { get; set; }
+    public int? Konsistens { get; set; }
 
-    public int Grabbvolum { get; set; }
+    public int? Grabbvolum { get; set; }
 
-    public int Tykkelseslamlag { get; set; }
+    public int? Tykkelseslamlag { get; set; }
 
-    public int Gassbobler { get; set; }
+    public int? Gassbobler { get; set; }
 }
