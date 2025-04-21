d3704eca9e92b003159665099739ef2274c8de5c
Isak Eriksen <eriksenly@icloud.com>
2025-04-21 22:02:52 +0200

    Lay out is finished

---
+++ SeaEco.Abstractions/Models/Project/BPositionAndDepthDto.cs
@@ -0,0 +1,10 @@
+namespace SeaEco.Abstractions.Models.Project;
+
+public class BPositionAndDepthDto
+{
+    public double? Kordinatern { get; set; }
+
+    public double? Kordinatero { get; set; }
+    
+    public double? Dybde  { get; set; }
+}
