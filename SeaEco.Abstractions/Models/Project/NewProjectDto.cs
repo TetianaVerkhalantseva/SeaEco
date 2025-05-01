fa5845761d8f84c87aa712a552836bd25fa92a5a
shv006 <97917772+shv006@users.noreply.github.com>
2025-05-01 11:03:05 +0200

    commmon custom buttons + addjust filds

--- SeaEco.Abstractions/Models/Project/NewProjectDto.cs
+++ SeaEco.Abstractions/Models/Project/NewProjectDto.cs
@@ -7,14 +7,15 @@ public class NewProjectDto
     public string PoId { get; set; } = null!;
     public Guid KundeId { get; set; }
     public string Kundekontaktperson { get; set; } = null!;
-    public string Kundetlf { get; set; }
+    public string? Kundetlf { get; set; } = null!;
     public string Kundeepost { get; set; } = null!;
-    
-    public Guid? Lokalitetid { get; set; }
+    public string Lokalitetsnavn { get; set; } = null!;
+    public string LokalitetsId { get; set; } = null!;
     public int Mtbtillatelse { get; set; }
     
     public Guid ProsjektansvarligId { get; set; }
     public string? Merknad { get; set; }
     public Produksjonsstatus Produksjonsstatus { get; set; }
     public DateTime Datoregistrert { get; set; }
+    public int AntallStasjoner { get; set; }
 }
--- SeaEco.Abstractions/Models/Project/ProjectDto.cs
+++ SeaEco.Abstractions/Models/Project/ProjectDto.cs
@@ -5,22 +5,22 @@ namespace SeaEco.Abstractions.Models.Project;
 public class ProjectDto
 {
     public Guid Id { get; set; }
-    
-    public string ProjectIdSe { get; set; }
     public string PoId { get; set; } = null!;
     public Guid KundeId { get; set; }
     public string Kundekontaktperson { get; set; } = null!;
     public string Kundetlf { get; set; } = null!;
     public string Kundeepost { get; set; } = null!;
     
-    public Guid Lokalitetid { get; set; }
+    public Guid LokalitetId { get; set; } 
+    public string Lokalitetsnavn { get; set; } = null!;
+    public string LokalitetsId { get; set; } = null!;
     public int? Mtbtillatelse { get; set; }
     
     public Guid ProsjektansvarligId { get; set; }
     public string? Merknad { get; set; }
-
-    public Prosjektstatus Prosjektstatus { get; set; }
+    
     public Produksjonsstatus Produksjonsstatus { get; set; }
     public DateTime Datoregistrert { get; set; }
+    public int AntallStasjoner { get; set; }
     
 }
