0677eab5e8c235c0626de84ee016017273f0e554
Therese IV <theresebvik@gmail.com>
2025-05-06 20:35:04 +0200

    Added preInfoService and Controller, projectIdSe-method and Antall Stasjoner placeholder.

---
+++ SeaEco.Abstractions/Models/PreInfo/PreInfoDto.cs
@@ -0,0 +1,18 @@
+﻿namespace SeaEco.Abstractions.Models.PreInfo;
+
+public class PreInfoDto
+{
+    public Guid Id { get; set; }
+    public Guid ProsjektId { get; set; }
+    public DateTime Feltdato { get; set; }
+    public Guid FeltansvarligId { get; set; }
+    public List<Guid> ProvetakerIds { get; set; } = new();
+    public float Ph { get; set; }
+    public float Eh { get; set; }
+    public float Temperatur { get; set; }
+    public float? RefElektrode { get; set; } 
+    public string Grabb { get; set; } = null!;
+    public string Sil { get; set; } = null!;
+    public string PhMeter { get; set; } = null!;
+    public DateOnly Kalibreringsdato { get; set; }
+}
