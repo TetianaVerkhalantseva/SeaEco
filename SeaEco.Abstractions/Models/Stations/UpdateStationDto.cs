c59e36cb98b46095b60cabf90881d421b64c2b15
shv006 <97917772+shv006@users.noreply.github.com>
2025-05-10 14:56:42 +0200

    slette + fungerende logikk ellers

--- SeaEco.Abstractions/Models/Stations/UpdateStationDto.cs
+++ SeaEco.Abstractions/Models/Stations/UpdateStationDto.cs
@@ -2,10 +2,15 @@
 
 public class UpdateStationDto
 {
-    public int NorthDegree { get; }
-    public float NorthMinutes { get; }
-    public int EastDegree { get; }
-    public float EastMinutes { get; }
+    public Guid Id { get; set; }
+    public int NorthDegree { get; set; }
+    public float NorthMinutes { get; set; }
+    public int EastDegree { get; set; }
+    public float EastMinutes { get; set; }
     public int Dybde { get; set; }
     public string Analyser { get; set; } = "Parameter I, II og III";
+    
+    public int? Nummer { get; set; }
+    public string? KoordinatNord { get; set; } 
+    public string? KoordinatOst { get; set; }
 }
