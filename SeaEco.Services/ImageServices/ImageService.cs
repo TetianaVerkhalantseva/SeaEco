6eec1638946f747839c22f4a157bfae18a2bcdb7
Tetiana <tda072@uit.no>
2025-05-11 18:26:45 +0200

    Added grouping of photos into folders based on ProsjektIdSe

--- SeaEco.Services/ImageServices/ImageService.cs
+++ SeaEco.Services/ImageServices/ImageService.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using SeaEco.Abstractions.Models.Image;
 using SeaEco.Abstractions.ResponseService;
 using SeaEco.EntityFramework.Entities;
@@ -13,14 +14,18 @@ public sealed class ImageService : IImageService
     private const string CANT_SAVE_IMAGE_ERROR = "Can't save image error";
     private const string CANT_REMOVE_IMAGE_ERROR = "Can't remove image error";
     private const string IMAGE_NOT_FOUND_ERROR = "Image not found";
+    private const string PROJECT_NOT_FDOUND_ERROR = "Project not found";
     
     private readonly IGenericRepository<BBilder> _imageRepository;
+    private readonly IGenericRepository<BUndersokelse> _undersokelRepository;
     private readonly string _destinationPath;
 
     public ImageService(IGenericRepository<BBilder> imageRepository,
+        IGenericRepository<BUndersokelse> undersokelRepository,
         IWebHostEnvironment hostingEnvironment)
     {
         _imageRepository = imageRepository;
+        _undersokelRepository = undersokelRepository;
         _destinationPath = Path.Combine(hostingEnvironment.WebRootPath, "images");
     }
     
@@ -28,9 +33,12 @@ public sealed class ImageService : IImageService
     {
         Guid imageId = Guid.NewGuid();
         
-        BBilder? dbRecords = await _imageRepository.GetBy(_ => _.UndersokelseId == dto.UndersokelseId &&
-                                                               _.Silt == dto.Silt);
-
+        BBilder? dbRecords = await _imageRepository.GetAll()
+            .Include(_ => _.Undersokelse)
+            .ThenInclude(_ => _.Prosjekt)
+            .FirstOrDefaultAsync(_ => _.UndersokelseId == dto.UndersokelseId &&
+                                      _.Silt == dto.Silt);
+        
         if (dbRecords is not null)
         {
             Response removeResult = await DeleteImage(dbRecords.Id);
@@ -40,7 +48,16 @@ public sealed class ImageService : IImageService
             }
         }
         
-        Response<ImageModel> uploadResult = SaveImage(dto.Image, imageId.ToString());
+        BUndersokelse? undersokelse = await _undersokelRepository.GetAll()
+            .Include(_ => _.Prosjekt)
+            .FirstOrDefaultAsync(_ => _.Id == dto.UndersokelseId);
+        
+        if (undersokelse is null)
+        {
+            return Response.Error(PROJECT_NOT_FDOUND_ERROR);
+        }
+        
+        Response<ImageModel> uploadResult = SaveImage(dto.Image, undersokelse.Prosjekt.ProsjektIdSe, imageId.ToString());
         if (uploadResult.IsError)
         {
             return Response.Error(uploadResult.ErrorMessage);
@@ -58,21 +75,24 @@ public sealed class ImageService : IImageService
         Response addResult = await _imageRepository.Add(dbRecord);
         if (addResult.IsError)
         {
-            RemoveImage(uploadResult.Value.Name);
+            RemoveImage(dbRecords.Undersokelse.Prosjekt.ProsjektIdSe, uploadResult.Value.Name);
         }
         return addResult;
     }
 
     public async Task<Response> DeleteImage(Guid id)
     {
-        BBilder? dbRecord = await _imageRepository.GetBy(record => record.Id == id);
+        BBilder? dbRecord = await _imageRepository.GetAll()
+            .Include(_ => _.Undersokelse)
+            .ThenInclude(_ => _.Prosjekt)
+            .FirstOrDefaultAsync(_ => _.Id == id);
+            
         if (dbRecord is null)
         {
-            RemoveImage(id.ToString());
             return Response.Error(IMAGE_NOT_FOUND_ERROR);
         }
 
-        Response removeResult = RemoveImage(dbRecord.Id.ToString());
+        Response removeResult = RemoveImage(dbRecord.Undersokelse.Prosjekt.ProsjektIdSe, dbRecord.Id.ToString());
         if (removeResult.IsError)
         {
             return removeResult;
@@ -83,26 +103,32 @@ public sealed class ImageService : IImageService
 
     public async Task<Response<ImageDto>> GetImage(Guid id)
     {
-        BBilder? dbRecord = await _imageRepository.GetBy(record => record.Id == id);
+        BBilder? dbRecord = await _imageRepository.GetAll()
+            .Include(_ => _.Undersokelse)
+            .ThenInclude(_ => _.Prosjekt)
+            .FirstOrDefaultAsync(_ => _.Id == id);
+        
         if (dbRecord is null)
         {
             return Response<ImageDto>.Error(IMAGE_NOT_FOUND_ERROR);
         }
 
+        string projectIdSe = dbRecord.Undersokelse.Prosjekt.ProsjektIdSe;
+        
         return Response<ImageDto>.Ok(new ImageDto()
         {
             Id = dbRecord.Id,
             UploadDate = dbRecord.Datogenerert,
             Silt = dbRecord.Silt,
-            Path = Path.Combine("/images", $"{dbRecord.Id.ToString()}.{dbRecord.Extension}"),
+            Path = Path.Combine("/images", projectIdSe, $"{dbRecord.Id.ToString()}.{dbRecord.Extension}"),
         });
     }
     
-    private Response<ImageModel> SaveImage(IFormFile image, string newName)
+    private Response<ImageModel> SaveImage(IFormFile image, string projectIdSe, string newName)
     {
-        if (!Directory.Exists(_destinationPath))
+        if (!Directory.Exists(Path.Combine(_destinationPath, projectIdSe)))
         {
-            Directory.CreateDirectory(_destinationPath);
+            Directory.CreateDirectory(Path.Combine(_destinationPath, projectIdSe));
         }
 
         string[] originalImageNameParts = image.FileName.Split('.');
@@ -111,7 +137,7 @@ public sealed class ImageService : IImageService
 
         try
         {
-            using (FileStream fileStream = new FileStream(Path.Combine(_destinationPath, newFileName), FileMode.Create))
+            using (FileStream fileStream = new FileStream(Path.Combine(_destinationPath, projectIdSe,  newFileName), FileMode.Create))
             {
                 image.CopyTo(fileStream);
             }
@@ -128,14 +154,14 @@ public sealed class ImageService : IImageService
         });
     }
 
-    private Response RemoveImage(string name)
+    private Response RemoveImage(string projectIdSe, string name)
     {
-        if (!Directory.Exists(_destinationPath))
+        if (!Directory.Exists(Path.Combine(_destinationPath, projectIdSe)))
         {
             return Response.Ok();
         }
 
-        string[] files = Directory.GetFiles(_destinationPath);
+        string[] files = Directory.GetFiles(Path.Combine(_destinationPath, projectIdSe));
 
         foreach (string file in files)
         {
@@ -145,7 +171,7 @@ public sealed class ImageService : IImageService
             {
                 try
                 {
-                    string filePath = Path.Combine(_destinationPath, fullFileName);
+                    string filePath = Path.Combine(_destinationPath, projectIdSe, fullFileName);
                     if (File.Exists(filePath))
                     {
                         File.Delete(filePath);
